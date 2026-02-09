b6dac3602675bfc9845b288b74fee25a4acffb68
Erick <erickcarpesmmc@gmail.com>
2026-02-09 15:57:34 -0300

    signup and login routes

---
+++ src/routes/signup/index.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+import SignupPage from '@/pages/SignupPage';
+
+export const Route = createFileRoute('/signup/')({
+  component: SignupPage,
+});
