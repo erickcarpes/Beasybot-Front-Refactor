0e7d00f97d08cac2d0f1cb380e6d8ce2aa10f64c
Erick <erickcarpesmmc@gmail.com>
2026-02-10 14:12:55 -0300

    fixing import in barrel file

--- src/services/beasybox-api/index.ts
+++ src/services/beasybox-api/index.ts
@@ -1,6 +1,6 @@
 export { fetchAccount, useAccount } from './account';
 export type { Account } from './account';
-export { AccountStatusEnum, AccountTierEnum } from './account';
+export { type AccountStatusEnum, type AccountTierEnum } from './account';
 // Services barrel export
 export { default as api, apiAuth } from './api';
 export { login, logout, refreshTokens } from './auth';
@@ -14,4 +14,4 @@ export {
 } from './tokenManager';
 export { fetchUser, useUser } from './user';
 export type { User } from './user';
-export { RoleEnum } from './user';
+export { type RoleEnum } from './user';
