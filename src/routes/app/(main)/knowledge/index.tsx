a43ef0b1a9f594c78cda78c7a39e6d1885c38203
Erick <erickcarpesmmc@gmail.com>
2026-02-10 12:00:32 -0300

    knowledge index

---
+++ src/routes/app/(main)/knowledge/index.tsx
@@ -0,0 +1,7 @@
+import { createFileRoute } from '@tanstack/react-router';
+
+import KnowledgePage from '@/pages/KnowledgePage';
+
+export const Route = createFileRoute('/app/(main)/knowledge/')({
+  component: KnowledgePage,
+});
