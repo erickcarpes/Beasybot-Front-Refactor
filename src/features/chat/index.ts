05299f332fb31f1e056904520ff6bae60f0d7d96
Erick <erickcarpesmmc@gmail.com>
2026-02-10 16:01:11 -0300

    chat barrel file

---
+++ src/features/chat/index.ts
@@ -0,0 +1,9 @@
+export { default as BulkDeleteChatModal } from './components/BulkDeleteChatModal';
+export {
+  type Chat,
+  type ChatOrigin,
+  useDeleteChat,
+  useDeleteChats,
+  useGetAllChats,
+  useUpdateChat,
+} from './services/chat';
