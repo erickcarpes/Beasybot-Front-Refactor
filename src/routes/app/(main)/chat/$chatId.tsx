261e7df5836c3d03b722417287727e1e748e688f
Erick <erickcarpesmmc@gmail.com>
2026-02-11 16:21:25 -0300

    dynamic route chatId

--- src/routes/app/(main)/chat/$chatId.tsx
+++ src/routes/app/(main)/chat/$chatId.tsx
@@ -2,6 +2,6 @@ import { createFileRoute } from '@tanstack/react-router';
 
 import ChatPage from '@/pages/ChatPage';
 
-export const Route = createFileRoute('/app/(main)/chat/')({
+export const Route = createFileRoute('/app/(main)/chat/$chatId')({
   component: ChatPage,
 });
