ac9e07ce00f5f04e5909aeddf47d06639108109c
Erick <erickcarpesmmc@gmail.com>
2026-02-11 16:26:35 -0300

    beggining of chat page

--- src/pages/ChatPage.tsx
+++ src/pages/ChatPage.tsx
@@ -1,3 +1,25 @@
+import { useCurrentUser } from '@/contexts/user/userContext';
+import MessageList from '@/features/chat/components/MessageList';
+import { useChat } from '@/features/chat/hooks/useChat';
+import ChatInput from '@/components/chat/ChatInput';
+import { Route } from '@/routes/app/(main)/chat/$chatId';
+
 export default function ChatPage() {
-  return <div>ChatPage</div>;
+  const { chatId } = Route.useParams();
+  const user = useCurrentUser();
+  const { isLoading, messages, sendMessage } = useChat({ chatId, userId: user.id });
+
+  return (
+    <div className="flex h-full flex-col">
+       <div className="flex-1 overflow-y-auto">
+        <MessageList messages={messages} />
+       </div>
+       <div className="flex justify-center p-4">
+        <ChatInput
+            onSendMessage={({ files, text }) => sendMessage(text, files)}
+            sending={isLoading}
+        />
+       </div>
+    </div>
+  );
 }
