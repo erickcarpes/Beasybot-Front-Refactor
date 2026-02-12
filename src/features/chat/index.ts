ede43c718b57ea0b8ff8e0ea41aa8556f09d1ac9
Erick <erickcarpesmmc@gmail.com>
2026-02-12 15:55:14 -0300

    chat barrel file

--- src/features/chat/index.ts
+++ src/features/chat/index.ts
@@ -1,7 +1,14 @@
 export { default as BulkDeleteChatModal } from './components/BulkDeleteChatModal';
+export { default as ChatInput } from './components/ChatInput';
+export { default as FileChip } from './components/FileChip';
+export { default as MessageBubble } from './components/MessageBubble';
+export { default as MessageList } from './components/MessageList';
+export { default as TypingIndicator } from './components/TypingIndicator';
+export { useChat } from './hooks/useChat';
 export {
   type Chat,
   type ChatOrigin,
+  useCreateChat,
   useDeleteChat,
   useDeleteChats,
   useGetAllChats,
