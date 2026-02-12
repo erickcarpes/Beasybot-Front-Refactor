c61623d55d7667eba1c84a307cc9c5c97d4004d2
Erick <erickcarpesmmc@gmail.com>
2026-02-12 15:53:28 -0300

    sticking sidebar to the right side

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -20,7 +20,7 @@ export default function AppLayout() {
           <MobileHeader />
 
           {/* Page content */}
-          <main className="flex-1 overflow-y-auto p-6">
+          <main className="flex-1 overflow-y-auto py-6">
             <Outlet />
           </main>
         </div>
