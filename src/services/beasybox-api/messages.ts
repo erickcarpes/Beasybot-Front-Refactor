068b366307fed8583d9bbb5b8ec28c7b93dad771
Erick <erickcarpesmmc@gmail.com>
2026-02-12 15:57:05 -0300

    message service

--- src/services/beasybox-api/messages.ts
+++ src/services/beasybox-api/messages.ts
@@ -1,15 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+
 import type { IFile } from './files';
 
-export type AuthorEnum = 'AGENT' | 'USER';
+import api from './api';
+
+export type AuthorEnum = 'BOT' | 'USER';
 
 export interface Message {
   author: AuthorEnum;
   chatId: string;
-  createdAt: Date | string;
+  createdAt: Date;
   files: IFile[];
   id?: string;
+  isStreaming?: boolean;
   status: MessageStatus;
   text: string;
 }
 
 export type MessageStatus = 'ANSWERED' | 'RECEIVED';
+
+export const useGetAllMessages = (chatId: string) => {
+  return useQuery({
+    queryFn: async () => {
+      const response = await api.get<Message[]>(`/message/${chatId}`);
+      return response.data;
+    },
+    queryKey: ['messages', chatId],
+  });
+};
